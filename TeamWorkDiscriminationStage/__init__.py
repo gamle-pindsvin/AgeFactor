f4ef40a7b5a3e7ed82fe3384f532b3ab16f490bf
gamle-pindsvin <det@gamle-pindsvin.de>
2025-05-01 15:33:53 +0200

    Aufteilung auf 4 Rollen, sonst noch keine Änderungen in TeamWorkDiscriminationStage

--- TeamWorkDiscriminationStage/__init__.py
+++ TeamWorkDiscriminationStage/__init__.py
@@ -12,8 +12,8 @@ from uvicorn import Config
 
 class Constants(BaseConstants):
     name_in_url = 'TeamWorkDiscriminationStage'
-    #TODO: MUSS MEHR SEIN, als die tatsächlich mögliche Anzahl der Teilnehmer also deutlich mehr als 12!!!
-    players_per_group = 12
+    # TODO: MUSS MEHR SEIN, als die tatsächlich mögliche Anzahl der Teilnehmer also deutlich mehr als 12!!!
+    players_per_group = 8
     num_rounds = 1
     # Wie lange soll die Aufgabe gelöst werden
     dauerDesThreatmentsInSekunden = 420
@@ -27,6 +27,22 @@ class Constants(BaseConstants):
     waehrungsFaktor = 1
     waehrungsName = 'GBP'
 
+    # players_per_group = 24
+
+    # Es gibt 4 Gruppen:
+    #   1 - Alt und mit einem Histogramm
+    #   2 - Jung und mit einem Histogramm
+    #   3 - Alt und mit ZWEI Histogrammen
+    #   4 - Jung und mit ZWEI Histogrammen
+
+    # Die Anzahl der Teilnehmer muss in allen 4 Gruppen gleich sein
+    # maximaleAnzahlProGruppe = players_per_group / 4
+
+    # ... soll aber jetzt dynamisch sein. Es müssen aber Vierfache sein, damit man auf Sequenzen aufteilen kann
+    maximaleAnzahlProGruppeT1 = 2
+    maximaleAnzahlProGruppeT2 = 2
+    maximaleAnzahlProGruppeT3 = 2
+    maximaleAnzahlProGruppeT4 = 2
 
 
 class Subsession(BaseSubsession):
@@ -35,28 +51,38 @@ class Subsession(BaseSubsession):
 
 class Group(BaseGroup):
 
-    pass
+    # Zähle Spieler, die schon der Gruppen/Rollen zugeordnet wurden.
+    anzahlSpielerInDerGruppe1 = models.IntegerField(initial=Constants.maximaleAnzahlProGruppeT1)
+    anzahlSpielerInDerGruppe2 = models.IntegerField(initial=Constants.maximaleAnzahlProGruppeT2)
+    anzahlSpielerInDerGruppe3 = models.IntegerField(initial=Constants.maximaleAnzahlProGruppeT3)
+    anzahlSpielerInDerGruppe4 = models.IntegerField(initial=Constants.maximaleAnzahlProGruppeT4)
 
 
 class Player(BasePlayer):
 
-    ProlificID = models.StringField( label='')
+    ProlificID = models.StringField(label='')
     AnzahlRichtigerAntworten = models.IntegerField(initial=0)
 
-    QuizKodieren1 = models.StringField(choices=[['a', 'a'], ['b', 'b'], ['c', 'c']], widget=widgets.RadioSelect, label='')
-    QuizKodieren2 = models.StringField(choices=[['a', 'a'], ['b', 'b'], ['c', 'c']], widget=widgets.RadioSelect, label='')
-    QuizBezahlung = models.StringField(choices=[['a', 'a'], ['b', 'b'], ['c', 'c']], widget=widgets.RadioSelect, label='')
+    QuizKodieren1 = models.StringField(choices=[['a', 'a'], ['b', 'b'], [
+                                       'c', 'c']], widget=widgets.RadioSelect, label='')
+    QuizKodieren2 = models.StringField(choices=[['a', 'a'], ['b', 'b'], [
+                                       'c', 'c']], widget=widgets.RadioSelect, label='')
+    QuizBezahlung = models.StringField(choices=[['a', 'a'], ['b', 'b'], [
+                                       'c', 'c']], widget=widgets.RadioSelect, label='')
 
     # Statistik am Ende
-    #PoliticalOrientation = models.StringField(choices=[['1', 'Very left-leaning'], ['2', 'Left-leaning'], ['3', 'Slightly left-leaning'], ['4', 'Centrist'], ['5', 'Slightly right-leaning'], ['6', 'Right-leaning'], ['7', "Very right-leaning"]], widget=widgets.RadioSelect, label='Where would you place yourself on the following political spectrum?')
-    Education = models.StringField(choices=[['1', 'No formal qualifications'], ['2', 'GCSEs or equivalent (e.g., O-Levels)'], ['3', 'A-Levels or equivalent (e.g., high school diploma)'], ['4', 'Vocational qualification (e.g., NVQ, BTEC)'], ['5', 'Undergraduate degree (e.g., BA, BSc)'], ['6', 'Postgraduate degree (e.g., MA, MSc, PhD)'], ['0', "Other "]], widget=widgets.RadioSelect, label='What is the highest level of education you have completed? ')
-    GeburtsJahr = models.IntegerField(min=1930, max=2010, label='In which year were you born?')
-    Gender = models.StringField(choices=[['F', 'Female'], ['M', 'Male'], ['D', 'Not listed'], ['N', 'Prefer not to answer']], widget=widgets.RadioSelect, label='What is your gender?')
+    # PoliticalOrientation = models.StringField(choices=[['1', 'Very left-leaning'], ['2', 'Left-leaning'], ['3', 'Slightly left-leaning'], ['4', 'Centrist'], ['5', 'Slightly right-leaning'], ['6', 'Right-leaning'], ['7', "Very right-leaning"]], widget=widgets.RadioSelect, label='Where would you place yourself on the following political spectrum?')
+    Education = models.StringField(choices=[['1', 'No formal qualifications'], ['2', 'GCSEs or equivalent (e.g., O-Levels)'], ['3', 'A-Levels or equivalent (e.g., high school diploma)'], ['4', 'Vocational qualification (e.g., NVQ, BTEC)'], [
+                                   '5', 'Undergraduate degree (e.g., BA, BSc)'], ['6', 'Postgraduate degree (e.g., MA, MSc, PhD)'], ['0', "Other "]], widget=widgets.RadioSelect, label='What is the highest level of education you have completed? ')
+    GeburtsJahr = models.IntegerField(
+        min=1930, max=2010, label='In which year were you born?')
+    Gender = models.StringField(choices=[['F', 'Female'], ['M', 'Male'], ['D', 'Not listed'], [
+                                'N', 'Prefer not to answer']], widget=widgets.RadioSelect, label='What is your gender?')
 
-    #Am Ende ggf. mit real_world_currency_per_point im Config korrigieren
-    #AuszahlungInWaehrung = models.IntegerField(initial=0)
+    # Am Ende ggf. mit real_world_currency_per_point im Config korrigieren
+    # AuszahlungInWaehrung = models.IntegerField(initial=0)
     VerdientePunkte = models.FloatField(initial=0.0)
-    AuszahlungWaehrungName = models.StringField();
+    AuszahlungWaehrungName = models.StringField()
 
     # ProlificID - Fehlerhinweis kann noch angezeigt werden
     HinweisLangeProlificID = models.BooleanField(initial=True)
@@ -66,7 +92,28 @@ class Player(BasePlayer):
     HatSichQualifiziert = models.BooleanField(initial=True)
 
     # Checkbox für Consent
-    Akzeptiert_bedingungen = models.BooleanField(label="", widget=widgets.CheckboxInput, blank=True)
+    Akzeptiert_bedingungen = models.BooleanField(
+        label="", widget=widgets.CheckboxInput, blank=True)
+    
+    zugeordneteRole = models.IntegerField()
+    
+    eintrittZeitAufDerSeite = models.FloatField(initial=-1.0)
+
+    # Prüfen, ob man selbst decodieren kann
+    SelbstTest = models.StringField(label='')
+
+    # ProlificID - Fehlerhinweis kann noch angezeigt werden
+    HinweisLangeProlificID = models.BooleanField(initial=True)
+
+    # Zählt falsche Versuche beim Quiz
+    FreiVersucheImQuiz = models.IntegerField(initial=2)
+    HatSichQualifiziert = models.BooleanField(initial=True)
+    FreiVersucheImQuiz2 = models.IntegerField(initial=2)
+    HatSichQualifiziert2 = models.BooleanField(initial=True)
+
+    # Hat "More Details" zur Auszahlung angeschaut
+    HatMoreDetailsAngeschaut = models.IntegerField(initial=0)
+
 
 
 class Intro(Page):
@@ -80,23 +127,59 @@ class Intro(Page):
     def is_displayed(player: Player):
         return player.round_number == 1
 
-    def error_message(player, values) :
-        if len(values['ProlificID']) != 24 and player.HinweisLangeProlificID :
+    def error_message(player, values):
+        if len(values['ProlificID']) != 24 and player.HinweisLangeProlificID:
             # nächstes Mal wird nicht nehr gewarnt
             player.HinweisLangeProlificID = False
-            #TODO - Text ggf. anpassen!
+            # TODO - Text ggf. anpassen!
             return 'Are you sure? Prolific-ID is normally 24 characters long.'
 
     @staticmethod
     def before_next_page(player: Player, timeout_happened):
 
         session = player.session
-        player.participant.ProlificID = player.ProlificID;
-        #print('Participant', player.participant.ProlificID, ' Player ID: ', player.ProlificID)
+        player.participant.ProlificID = player.ProlificID
+        # print('Participant', player.participant.ProlificID, ' Player ID: ', player.ProlificID)
+
+        gruppe = player.group
+
+        # Es gibt 4 Rollen:
+        #   1 - Alt und mit einem Histogramm
+        #   2 - Jung und mit einem Histogramm
+        #   3 - Alt und mit ZWEI Histogrammen
+        #   4 - Jung und mit ZWEI Histogrammen
+        # Wir bleiben solange in einer Schleife, bis der Probant einer Rolle zugeordnet ist
+        # War für eine Rolle schon genug Teilnehmer zugeordnet, wird neue Zufallszahl generiert
+
+        weiterRolleSuchen = True
+        while weiterRolleSuchen:
+            zufallszahl = random.randint(1, 4)
+
+            # print("Intro: zufallszahl: ", zufallszahl)
+
+            # Kompakte Darstellung, für
+            #       if gruppe.anzahlSpielerInDerGruppe1 >=1:
+            #       gruppe.anzahlSpielerInDerGruppe1 = gruppe.anzahlSpielerInDerGruppe1-1 ...
+            gruppen_attribut = f'anzahlSpielerInDerGruppe{zufallszahl}'
+            if getattr(gruppe, gruppen_attribut) >= 1:
+                setattr(gruppe, gruppen_attribut, getattr(gruppe, gruppen_attribut) - 1)
+
+                player.zugeordneteRole = zufallszahl
+                player.participant.zugeordneteRole = zufallszahl
+                weiterRolleSuchen = False
+
+
+        print("Rolle: ", player.zugeordneteRole)
+        print("Neue freie Rollen: [", gruppe.anzahlSpielerInDerGruppe1, ", " , gruppe.anzahlSpielerInDerGruppe2, ", " ,gruppe.anzahlSpielerInDerGruppe3, ", " ,gruppe.anzahlSpielerInDerGruppe4, "]")
+
+        # TODO HIER NUR UM ALLES IN T2 zu testen - DANACH LÖSCHEN!!!!
+        # player.zugeordneteRole = 2;
+        # player.participant.zugeordneteRole = 2;
+
         # Neues Spiel - neue Auszahlung.
         player.payoff = 0
         player.participant.payoff = 0
-        #player.AuszahlungInWaehrung = 0
+        # player.AuszahlungInWaehrung = 0
         player.VerdientePunkte = 0.0
         player.participant.VerdientePunkte = 0.0
         player.AnzahlRichtigerAntworten = 0
@@ -112,41 +195,44 @@ class Intro2(Page):
     def error_message(player, values):
         # print(values['Akzeptiert_bedingungen'])
         # Checkbox nicht ausgewählt
-        if ( not (values['Akzeptiert_bedingungen'])):
-           return 'Please confirm that you have read the instructions and the Informed Consent'
+        if (not (values['Akzeptiert_bedingungen'])):
+            return 'Please confirm that you have read the instructions and the Informed Consent'
 
 # Verständnis-Quiz
+
 class Quiz(Page):
     form_model = 'player'
     form_fields = ["QuizKodieren1", "QuizKodieren2", "QuizBezahlung"]
 
-
     @staticmethod
     def error_message(player, values):
         # Wenn player.FreiVersucheImQuiz 0 werden, geht weiter, aber gleich zu der Auszahlungsseite.
-        quizKorrekt = ((values['QuizKodieren1'] == 'b') and (values['QuizKodieren2'] == 'c') and (values['QuizBezahlung'] == 'b'))
-        #print(values['QuizKodieren1'], 'bool: ', (values['QuizKodieren1'] == 'B'))
-        #print(values['QuizKodieren2'], 'bool: ', (values['QuizKodieren2'] == 'C'))
-        #print(values['QuizBezahlung'], 'bool: ', (values['QuizBezahlung'] == 'C'))
+        quizKorrekt = ((values['QuizKodieren1'] == 'b') and (
+            values['QuizKodieren2'] == 'c') and (values['QuizBezahlung'] == 'b'))
+        # print(values['QuizKodieren1'], 'bool: ', (values['QuizKodieren1'] == 'B'))
+        # print(values['QuizKodieren2'], 'bool: ', (values['QuizKodieren2'] == 'C'))
+        # print(values['QuizBezahlung'], 'bool: ', (values['QuizBezahlung'] == 'C'))
         if not quizKorrekt:
-            #print('NICHT KORREKT player.FreiVersucheImQuiz war: ', player.FreiVersucheImQuiz, " / quizKorrekt: ", quizKorrekt)
+            # print('NICHT KORREKT player.FreiVersucheImQuiz war: ', player.FreiVersucheImQuiz, " / quizKorrekt: ", quizKorrekt)
             player.FreiVersucheImQuiz -= 1
-            if player.FreiVersucheImQuiz > 0 :
-                #print('Try again please. One or both answers are not yet correct. You have ', str(player.FreiVersucheImQuiz), ' attempts left.')
+            if player.FreiVersucheImQuiz > 0:
+                # print('Try again please. One or both answers are not yet correct. You have ', str(player.FreiVersucheImQuiz), ' attempts left.')
                 return 'Try again please. One or more answers are not yet correct. You have ' + str(player.FreiVersucheImQuiz) + ' attempts left.'
             else:
-                #keine Freiversuche mehr
+                # keine Freiversuche mehr
                 player.HatSichQualifiziert = False
         else:
-            #print('KORREKT!!')
+            # print('KORREKT!!')
             pass
 
     @staticmethod
     def vars_for_template(player: Player):
-        #Werte für die Auszahlungsfragen
+        # Werte für die Auszahlungsfragen
         antwortA = Constants.quizAngenommeneAnzahlAntowrten*Constants.bezahlungProAntwort
-        antwortB = Constants.quizAngenommeneAnzahlAntowrten*Constants.bezahlungProAntwort + Constants.festerAnteilderBezahlung
-        antwortC = Constants.quizAngenommeneAnzahlAntowrten + Constants.festerAnteilderBezahlung
+        antwortB = Constants.quizAngenommeneAnzahlAntowrten* \
+            Constants.bezahlungProAntwort + Constants.festerAnteilderBezahlung
+        antwortC = Constants.quizAngenommeneAnzahlAntowrten + \
+            Constants.festerAnteilderBezahlung
         return {
             'AntwortA': antwortA,
             'AntwortB': antwortB,
@@ -154,22 +240,20 @@ class Quiz(Page):
         }
 
 
-
     @staticmethod
     def before_next_page(player: Player, timeout_happened):
         pass
 
 
-
 class RealEffortTask(Page):
     form_model = 'player'
-    #form_fields = ["AnzahlRichtigerAntworten"]
+    # form_fields = ["AnzahlRichtigerAntworten"]
     timeout_seconds = Constants.dauerDesThreatmentsInSekunden
 
     @staticmethod
     # Wird bei denen NICHT anzgezeigt, die das Quiz nicht geschafft haben.
     def is_displayed(player: Player):
-        #print('HatSichQualifiziert: ', player.HatSichQualifiziert)
+        # print('HatSichQualifiziert: ', player.HatSichQualifiziert)
         return player.HatSichQualifiziert
 
     @staticmethod
@@ -187,10 +271,11 @@ class RealEffortTask(Page):
             # OK-Button
             player.participant.AnzahlRichtigerAntworten = data
             player.AnzahlRichtigerAntworten = data
-            #print('$$ 1 $$ player.participant.AnzahlRichtigerAntworten: ', player.participant.AnzahlRichtigerAntworten , ' player.AnzahlRichtigerAntworten: ', player.AnzahlRichtigerAntworten)
+            # print('$$ 1 $$ player.participant.AnzahlRichtigerAntworten: ', player.participant.AnzahlRichtigerAntworten , ' player.AnzahlRichtigerAntworten: ', player.AnzahlRichtigerAntworten)
 
             # BERECHNE die Auszahlung
-            auszahlung = Constants.festerAnteilderBezahlung + data*Constants.bezahlungProAntwort
+            auszahlung = Constants.festerAnteilderBezahlung + \
+                data*Constants.bezahlungProAntwort
             player.VerdientePunkte = auszahlung
             player.participant.VerdientePunkte = auszahlung
 
@@ -198,40 +283,47 @@ class RealEffortTask(Page):
             player.payoff = auszahlung * Constants.waehrungsFaktor
             player.participant.payoff = auszahlung * Constants.waehrungsFaktor
 
-            print('player.participant.ProlificID: ', player.participant.ProlificID, ' player.participant.AnzahlRichtigerAntworten: ', player.participant.AnzahlRichtigerAntworten, ' player.participant.VerdientePunkte: ', player.participant.VerdientePunkte)
+            print('player.participant.ProlificID: ', player.participant.ProlificID, ' player.participant.AnzahlRichtigerAntworten: ',
+                  player.participant.AnzahlRichtigerAntworten, ' player.participant.VerdientePunkte: ', player.participant.VerdientePunkte)
         else:
             # Abbrechen-Button
-            print('ABBRECHEN 1: ', ' player.participant.AnzahlRichtigerAntworten: ', player.participant.AnzahlRichtigerAntworten)
+            print('ABBRECHEN 1: ', ' player.participant.AnzahlRichtigerAntworten: ',
+                  player.participant.AnzahlRichtigerAntworten)
             # Die Auszahlung
 
-            print('$$ 22222222222222222222222222222222222222222222222222222222222222222222  $$')
+            print(
+                '$$ 22222222222222222222222222222222222222222222222222222222222222222222  $$')
             for key, value in player.participant.vars.items():
                 print(key, value)
 
-            print('$$ 22222222222222222222222222222222222222222222222222222222222222222222  ENDE $$')
+            print(
+                '$$ 22222222222222222222222222222222222222222222222222222222222222222222  ENDE $$')
 
             if 'VerdientePunkte' in player.participant.vars:
                 wert = player.participant.vars['VerdientePunkte']
-                print('$$ 22222222222222222222222222222222222222222222222222222222222222222222  $$ wert: ', wert)
+                print(
+                    '$$ 22222222222222222222222222222222222222222222222222222222222222222222  $$ wert: ', wert)
             else:
                 wert = None  # oder einen anderen Standardwert
 
 
-
-            #if player.participant.VerdientePunkte is None:
+            # if player.participant.VerdientePunkte is None:
             #    player.participant.VerdientePunkte = Constants.festerAnteilderBezahlung
             #    player.participant.AnzahlRichtigerAntworten = 0
 
-            print('ABBRECHEN 2: ', ' player.participant.AnzahlRichtigerAntworten: ', player.participant.AnzahlRichtigerAntworten, ' player.participant.VerdientePunkte: ', player.participant.VerdientePunkte)
-            print('ABBRECHEN: ', ' player.AnzahlRichtigerAntworten: ', player.AnzahlRichtigerAntworten, ' player.participant.VerdientePunkte: ', player.participant.VerdientePunkte)
+            print('ABBRECHEN 2: ', ' player.participant.AnzahlRichtigerAntworten: ', player.participant.AnzahlRichtigerAntworten,
+                  ' player.participant.VerdientePunkte: ', player.participant.VerdientePunkte)
+            print('ABBRECHEN: ', ' player.AnzahlRichtigerAntworten: ', player.AnzahlRichtigerAntworten,
+                  ' player.participant.VerdientePunkte: ', player.participant.VerdientePunkte)
             return {player.id_in_group: 'next_page'}
 
 # Auszahlung und Statistik werden vorbereitet
+
 class AuszahlungUmfrage(Page):
 
     form_model = 'player'
     # GeburtsJahr wird EXTRA aufgeführt!! Wenn nicht funktioniert, wie unten.
-    #form_fields = ['GeburtsJahr', 'Gender', 'Education', 'PoliticalOrientation']
+    # form_fields = ['GeburtsJahr', 'Gender', 'Education', 'PoliticalOrientation']
     form_fields = ['GeburtsJahr', 'Gender', 'Education']
 
     @staticmethod
@@ -243,10 +335,11 @@ class AuszahlungUmfrage(Page):
             player.payoff = player.VerdientePunkte * Constants.waehrungsFaktor
             player.participant.AnzahlRichtigerAntworten = 0
             player.participant.VerdientePunkte = Constants.festerAnteilderBezahlung
-            player.participant.payoff = player.participant.VerdientePunkte * Constants.waehrungsFaktor
-            #print('$$ Y $$ player.participant.VerdientePunkte: ', player.participant.VerdientePunkte )
-        #else:
-            #print('$$ N $$ player.participant.VerdientePunkte: ', player.participant.VerdientePunkte )
+            player.participant.payoff = player.participant.VerdientePunkte * \
+                Constants.waehrungsFaktor
+            # print('$$ Y $$ player.participant.VerdientePunkte: ', player.participant.VerdientePunkte )
+        # else:
+            # print('$$ N $$ player.participant.VerdientePunkte: ', player.participant.VerdientePunkte )
 
 
 # Ergebnis des Entscheidungsvorlage-Games wird angezeigt
